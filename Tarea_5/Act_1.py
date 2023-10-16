e3b51b4ec34d54eea2b05b3d41607c2fd0d65964
Toñin <j.antoniokumar@gmail.com>
2023-10-16 20:44:31 +0200

    Tarea 5 act.

---
+++ Tarea_5/Act_1.py
@@ -0,0 +1,7 @@
+import math
+
+A = int(input("Escribe la base de un triángulo: "))
+B = int(input("Escribe la altura de dicho triángulo: "))
+
+Area = B*A/2
+print("El área del triángulo es ",Area, "centímetros.")
---
+++ Tarea_5/Act_2.py
@@ -0,0 +1,5 @@
+import math
+
+
+A = int(input("Introduce un valor numérico: "))
+B = int(input("Introduce otro valor numérico: "))
