fdd119a15bb3fcdd6510207debeb9cf14d63f360
Toñin <j.antoniokumar@gmail.com>
2023-12-14 17:46:54 +0100

    parche

--- Minijuego 2/elementos2.py
+++ Minijuego 2/elementos2.py
@@ -5,7 +5,10 @@ class Nave (pygame.sprite.Sprite):
     def __init__(self, posicion) -> None:
         super().__init__()
         #cargamos la imagen
-        self.image = pygame.image.load("avion4.png")
+        self.imagenes = [pygame.image.load("avion4.png"), pygame.image.load("avion5.png")]
+        self.indice_imagen = 0
+        self.image = self.imagenes[self.indice_imagen]
+        self.contador_imagen = 0
         #creamos un rectangulo a partir de la imagen
         self.rect = self.image.get_rect()
         #actualizar la posición del rectangulo para que coincida con "posicion"
@@ -15,8 +18,32 @@ class Nave (pygame.sprite.Sprite):
     #update
     def update(self, *args: any, **kwargs: any) -> None:
         teclas = args[0]
+        #capturamos la pantalla
+        pantalla = pygame.display.get_surface()
         if teclas[pygame.K_LEFT]:
             self.rect.x -= 2
+            self.rect.x = max(0, self.rect.x)
         elif teclas[pygame.K_RIGHT]:
             self.rect.x += 2
-        pass
+            self.rect.x = min(pantalla.get_width() - self.image.get_width(), self.rect.x)
+        #gestionamos
+        self.contador_imagen = (self.contador_imagen + 1) % 40
+        self.indice_imagen = self.contador_imagen // 20
+        self.image = self.imagenes[self.indice_imagen]
+#creador de enemigos
+class Enemigo(pygame.sprite.Sprite):
+    def __init__(self, posicion) -> None:
+        super().__init__()
+        #cargamos la imagen
+        imagen = pygame.image.load("avion4.png")
+        self.image = pygame.transform.rotate(imagen, 180)
+        #creamos un rectangulo a partir de la imagen
+        self.rect = self.image.get_rect()
+        #actualizar la posición del rectangulo para que coincida con "posicion"
+        self.rect.topleft = posicion
+
+    def update(self, *args: any, **kwargs: any):
+        pantalla = pygame.display.get_surface()
+        self.rect.y += 1
+        self.rect.x = max(0, self.rect.x)
+        self.rect.x = min(pantalla.get_width() - self.image.get_width(), self.rect.x)
--- Minijuego 2/space2.py
+++ Minijuego 2/space2.py
@@ -1,6 +1,6 @@
 import pygame
 import elementos2
-
+import random
 #inicialicamos el juego
 pygame.init()
 
@@ -23,6 +23,13 @@ grupo_sprites.add(elementos2.Nave((400,200)))
 grupo_sprites.add(elementos2.Nave((500,100)))
 grupo_sprites.add(elementos2.Nave((100,300)))
 
+
+enemigo = elementos2.Enemigo((50,50))
+grupo_sprites.add(elementos2.Enemigo((70, 70)))
+
+#crear u na variable que almacene la última creacion de enemigo
+ultimo_enemigo_creado = 0
+
 #bucle principal
 while running:
     #Limitamos el bucle al framrate definido
@@ -31,6 +38,15 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+
+    #creacion de enemigos
+    momento_actual = pygame.time.get_ticks()
+   # if(momento_actual > ultimo_enemigo_creado + 180):
+    #    cordX = random.randint(0, pantalla.get_width())
+     #   cordY = 0
+      #  grupo_sprites.add(elementos2.Enemigo((cordX, cordY)))
+       # ultimo_enemigo_creado = momento_actual
 
     #capturamos las teclas
     teclas = pygame.key.get_pressed()
