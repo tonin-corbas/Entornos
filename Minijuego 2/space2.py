b955f9fb4cb0bd5d279c056ad7b82a038fc24aee
Toñin <j.antoniokumar@gmail.com>
2024-01-08 20:42:43 +0100

    parche

--- Minijuego 2/space2.py
+++ Minijuego 2/space2.py
@@ -1,6 +1,7 @@
 import pygame
 import elementos2
 import random
+import pygame_menu
 #inicialicamos el juego
 pygame.init()
 
@@ -13,7 +14,6 @@ reloj = pygame.time.Clock()
 FPS = 100
 
 #booleano de control
-running = [True]
 posicion = (650,700)
 nave = elementos2.Nave(posicion)
 fondo = elementos2.Fondo()
@@ -38,37 +38,61 @@ grupo_sprites_todos.add(nave)
 ultimo_enemigo_creado = 0
 frecuencia_creacion_enemigo = 1500
 
-#bucle principal
-while running:
-    #Limitamos el bucle al framrate definido
-    reloj.tick(FPS)
-    #gestionar la salida
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def set_difficulty(value, difficulty):
+    # Do the job here !
+    pass
 
+def start_the_game():
+    # Do the job here !
+    running = [True]
+    global ultimo_enemigo_creado
+    global frecuencia_creacion_enemigo
+    global FPS
+    global grupo_sprites_bala
+    global grupo_sprites_todos
+    global grupo_sprites_enemigos
+    global reloj
 
-    #creacion de enemigos
-    momento_actual = pygame.time.get_ticks()
-    if(momento_actual > ultimo_enemigo_creado + frecuencia_creacion_enemigo):
-        cordX = random.randint(0, pantalla.get_width())
-        cordY = 0
-        enemigo = elementos2.Enemigo((cordX, cordY))
-        grupo_sprites_todos.add(enemigo)
-        grupo_sprites_enemigos.add(enemigo)
-        ultimo_enemigo_creado = momento_actual
+    # bucle principal
+    while running[0]:
+        # Limitamos el bucle al framrate definido
+        reloj.tick(FPS)
+        # gestionar la salida
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = [False]
 
-    #capturamos las teclas
-    teclas = pygame.key.get_pressed()
-    #if teclas[pygame.K_SPACE]:
-     #   nave.disparar(grupo_sprites_todos)
+        # creacion de enemigos
+        momento_actual = pygame.time.get_ticks()
+        if (momento_actual > ultimo_enemigo_creado + frecuencia_creacion_enemigo):
+            cordX = random.randint(0, pantalla.get_width())
+            cordY = 0
+            enemigo = elementos2.Enemigo((cordX, cordY))
+            grupo_sprites_todos.add(enemigo)
+            grupo_sprites_enemigos.add(enemigo)
+            ultimo_enemigo_creado = momento_actual
 
-    #pintaremos:
-    #pantalla.fill((255,255,255))
-    grupo_sprites_todos.update(teclas, grupo_sprites_todos, grupo_sprites_bala, grupo_sprites_enemigos, running)
-    grupo_sprites_todos.draw(pantalla)
+        # capturamos las teclas
+        teclas = pygame.key.get_pressed()
+        if teclas[pygame.K_ESCAPE]:
+          running[0] = False
 
-    #redibujar la pantala
-    pygame.display.flip()
+        # pintaremos:
+        # pantalla.fill((255,255,255))
+        grupo_sprites_todos.update(teclas, grupo_sprites_todos, grupo_sprites_bala, grupo_sprites_enemigos, running)
+        grupo_sprites_todos.draw(pantalla)
+
+        # redibujar la pantala
+        pygame.display.flip()
+    pass
+
+menu = pygame_menu.Menu('Welcome', 400, 300, theme=pygame_menu.themes.THEME_BLUE)
+
+menu.add.text_input('Name :', default='John Doe')
+menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
+menu.add.button('Play', start_the_game)
+menu.add.button('Quit', pygame_menu.events.EXIT)
+
+menu.mainloop(pantalla)
 #finalizamos el juego
 pygame.quit()
